4508715472ea2f0b9595ebdd689b606bafbbdb6c
elecun <elecun2@gamil.com>
2022-03-08 09:31:57 +0900

    udpated for preprocessing

--- pyppe/tool_detector_image.py
+++ pyppe/tool_detector_image.py
@@ -7,38 +7,78 @@ import cv2
 from cv2 import COLOR_GRAY2BGR
 import numpy as np
 from cv2 import COLOR_RGB2GRAY
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
-    # 1. load source image
-    raw = cv2.imread("./experiments/0216_tool_1/38.jpg")
-    gray = cv2.cvtColor(raw, COLOR_RGB2GRAY)
+    # Load Images
+    raw = cv2.imread("./experiments/0216_tool_1/25.jpg")
+    tool = cv2.imread("./experiments/tool-line.jpg")
+    raw_gray = cv2.cvtColor(raw, COLOR_RGB2GRAY)
+    tool_gray = cv2.cvtColor(tool, COLOR_RGB2GRAY)
 
-    #camera calibration (for uc762c camera model)
-    h,w = gray.shape[:2]
+    # Show Histogram
+    hist = cv2.calcHist([raw_gray],[0],None,[256],[0,256])
+    plt.plot(hist)
+    plt.show()
+
+    # Camera Calibration (for Arducam US762C Camera Model)
+    h,w = raw_gray.shape[:2]
     mtx = np.matrix([[2517.792, 0., 814.045],[0., 2514.767, 567.330],[0., 0., 1.]])
     dist = np.matrix([[-0.361044, 0.154482, 0.000808, 0.000033, 0.]])
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    gray = cv2.undistort(gray, mtx, dist, None, newcameramtx)
-    
-    # change raw
-    out = cv2.cvtColor(gray, COLOR_GRAY2BGR)
-    monitor = np.zeros((h,w,3), np.uint8)
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h))
+    raw_gray = cv2.undistort(raw_gray, mtx, dist, None, newcameramtx)
 
-    # draw wafer circle
-    h,w = gray.shape[:2]
-    cv2.circle(monitor, (int(w/2), int(h/2)), 300, (0,255,0), 1)
-    
+
+    # create blank image
+    blank = np.zeros((h,w,3), np.uint8)
+
+    # equalization
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    #raw_gray = clahe.apply(raw_gray)
 
     #binarization
-    #bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 5)
-    ret, bin = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    raw_bin = cv2.adaptiveThreshold(raw_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
+
+    tool_bin = cv2.adaptiveThreshold(tool_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 5)
+    tool_bin = cv2.bitwise_not(tool_bin)
+
+    # apply contour
+    contours, hierarchy = cv2.findContours(raw_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        if cv2.contourArea(cnt)>500:
+            cv2.drawContours(blank, [cnt], 0, (255, 0, 0), 1)  # blue
+
+    #key point detection (ORB)---------------
+    # Initiate ORB detector
+    orb = cv2.ORB_create()
+    # find the keypoints with ORB
+    kp1, des1 = orb.detectAndCompute(raw_bin, None)
+    kp2, des2 = orb.detectAndCompute(tool_bin, None)
+    # draw only keypoints location,not size and orientation
+    raw_keypoints = cv2.drawKeypoints(raw_bin, kp1, None, color=(0,255,0), flags=0)
+    tool_keypoints = cv2.drawKeypoints(tool_bin, kp2, None, color=(0,255,0), flags=0)
+    #cv2.imshow("raw_keypoints", raw_keypoints)
+    #cv2.imshow("tool_keypoints", tool_keypoints)
+
+    #lsd detection
+    lsd = cv2.createLineSegmentDetector(0)
+    lines = lsd.detect(raw_bin)[0] #Position 0 of the returned tuple are the detected lines
+
+    #Draw detected lines in the image
+    drawn_img = lsd.drawSegments(blank,lines)
+
+    #Show image
+    cv2.imshow("LSD",drawn_img )
+
+    #ret, bin = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     # contour
-    contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
-    for cnt in contours:
-        if cv2.contourArea(cnt)>5000:
-            cv2.drawContours(out, [cnt], 0, (0, 255, 0), 3)  # blue
+    contours, hierarchy = cv2.findContours(raw_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
+    # for cnt in contours:
+    #     if cv2.contourArea(cnt)>500:
+    #         cv2.drawContours(out, [cnt], 0, (0, 255, 0), 3)  # blue
+    
     
     # for cnt in contours:
     #     epsilon = 0.02 * cv2.arcLength(cnt, True)
@@ -52,8 +92,7 @@ if __name__ == "__main__":
     # img = cv2.drawKeypoints(raw, keypoints, None)
     
 
-    cv2.imshow("result", out)
-    cv2.imshow("monitor", monitor)
+    
     while True:
         key = cv2.waitKey(0)
         if key == 27:
